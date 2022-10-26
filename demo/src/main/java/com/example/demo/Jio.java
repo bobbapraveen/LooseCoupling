182de47c2552634f4d71c8f205718b6407ff1d2c
bobbapraveen <praveenbobba>
2022-10-26 13:58:53 -0500

    Upadted File

--- demo/src/main/java/com/example/demo/Jio.java
+++ demo/src/main/java/com/example/demo/Jio.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Jio  {
     private Service service;
 
+    @Autowired
+    @Qualifier("ht")
     public void setService(Service service) {
         this.service = service;
     }
